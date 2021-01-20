822f34beb17c9d6b0dbdb5e2bf273ed5b336c18a
Rob Cole <robertncole1@gmail.com>
2021-01-19 20:17:23 -0600

    walked through loops

--- main.js
+++ main.js
@@ -1,1 +1,51 @@
 console.log('Hello World');
+
+// for (let i = 0; i <= 15; i++) {
+//   console.log(i);
+// }
+
+// const functionforTad = () => {
+//   for (let i = 0; i <= 15; i++) {
+//       console.log(i);
+//     }
+// }
+
+// functionforTad();
+
+// ********  ARRAYS WITH LOOPS
+// const ec14Instuctors = ['Aja', 'Trinity', 'Dr. T', 'Discussion Tickets'];
+
+// for (let i = 0; i < ec14Instuctors.length; i++) {
+//   console.log(ec14Instuctors[i]);
+// }
+
+// const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+
+// const colorLoop = () => {
+//   let domString = '';
+
+//   for (let i = 0; i < colors.length; i++) {
+//     domString += `<h1>${colors[i]}</h1>`
+//   }
+
+//   console.log(domString);
+// }
+
+// colorLoop();
+
+const instructors [
+  { first: 'Trinity', last: 'Christiana'},
+  { first: 'Aja', last: 'Washington'},
+  { first: 'Teresa', last: 'Vasquez'},
+];
+
+const nameLoop = () => {
+  let domString = '';
+
+  for (let i = 0; i < instructors.length; i++) {
+    domString += `<h1>${instructors[i].first} ${instructors[i].last}</h1>`;
+  }
+  console.log(domString);
+}
+
+nameLoop();
